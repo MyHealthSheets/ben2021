6fb3d98cec343929c1ef981691b5162fd6665daf
root <myhealthsheets>
2021-06-02 19:12:41 -0400

    some updates to index.html

--- docroot/home_tabs.php
+++ docroot/home_tabs.php
@@ -1,13 +1,28 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="scripts/myhs_styles.css"/>
+	<!-- <link rel="stylesheet" href="bootstrap-4.3.1-dist/scss/custom.scss"> -->
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>Ben's Website</title>
+</head>
+<body>
+
+
 <div class="container-fluid" style="margin-top:15rem">
-	<div class="my_tabs">
-	  <button class="tablinks" onclick="openEx(event, 'Education')"id="test"><h4>General Experience</h4></button>
-	  <button class="tablinks" onclick="openEx(event, 'Work')"><h4>Raspberry Pi</h4></button>
-	  <button class="tablinks" onclick="openEx(event, 'Strengths')"><h4>Web Applications</h4></button>
-	  <button class="tablinks" onclick="openEx(event, 'Hobbies')"><h4>Mobile Applications</h4></button>
+	<div class="tab">
+	  <button class="tablinks" onclick="openEx(event, 'general')"id="test"><h4>General Experience</h4></button>
+	  <button class="tablinks" onclick="openEx(event, 'raspi')"><h4>Raspberry Pi</h4></button>
+	  <button class="tablinks" onclick="openEx(event, 'web_apps')"><h4>Web Applications</h4></button>
+	  <button class="tablinks" onclick="openEx(event, 'mobile_apps')"><h4>Mobile Applications</h4></button>
 	</div>
 
 
-	<div id="Education" class="tab_content">
+	<div id="general" class="tab_content">
 		<div class="card">
 			<h4 class="card-header">Software Programming Diploma</h4>
 			<div class="card-body">
@@ -30,8 +45,129 @@
 		</div>
 	</div>
 
+	<div id="raspi" class="tab_content">
+		<div class="card">
+			<div class="card-header">
+				<h3>Some more Recent Raspbery Pi Projects</h3>
+			</div>
+			<div class="card-body">
+			<ul style="list-style-type:none">
+		 	<li> <h5>OpenHAB (Smart Home Server)</h5>
+				 <p> Open Source PHP Web Application is a tool to setup and control Smart Home
+				devices from a Web Browser. <br><a href="https://www.openhab.org/" Use target="_blank"> Click this link to learn more about OpenHAB</a>
+			  	</p><hr>
+				
+		  	<li>
+				<h5>Personal PHP Web site projects </h5>
+				<p>I have been developing a couple PHP based Web sites/applications for practice.
+				This site is a simple PHP application, however I also have a demo Web Application for myhealthsheets.com. I do most of my dev work on my Raspberry Pi, then push this to a Web Server when ready for production.
+				</p> 
+				<hr>
+			</li>
+			<li>
+				<h5>Personal Ebook Server using Calibre</h5>
+				<p>Browse, Download, Upload my book library and read direct from Web Browser
+				</p> 
+			</li><hr>  
+			<li>
+			<h5>Owncloud Server</h5>
+				<p> Personal File Server similar to Dropbox and other Applications. This allows me to share files with family and friends without any free tier storage size limitations.
+	                </li><hr>			
 
+			<li>
+			<h5>Druapal Web Application</h5>
+                        <p>Created some basic Drupal Applications for practice. 
+			<a href="https://www.drupal.org/" Use target="_blank"> Visit Drupal.org for
+			more details.</a>
+                        </p>
+                	</li><hr>
+	                <li>
+			 <h5>MiniDLNA, Plex and Kodi Media Servers</h5>
+                        <p>I worked various technologies like this in order to share media content across my home network. These applications allow any TV or monitor attached to the network to 
+			browse and watch any type of Media Content. <a href="https://pimylifeup.com/raspberrypi-minidlna/" Use target="_blank">This link will take you to a great doc on how to setup miniDLNA
+                        more details.</a>
+                        </p>
+                 	</li><hr>
+		 	<li>
+                 	<h5>RetroPIE - EmulationStation for Raspberry Pi</h5>
+			<p>This turns any Raspi into a retro-gaming machine. Allows you to play just about
+			all retro gaming consoles through your raspberry Pi.This is a fun project that you can
+			also setup with NES or other type cases and utilize BT joysticks that look like
+			retro controllers. 
+                        <a href="https://retropie.org.uk/" Use target="_blank">Start
+			here for more details.</a>
+                        </p>
+                 	</li><hr>
+			<h5>Python Scripts and other General Command Line Tools</h5>
+                        <p>I have worked and am working on many other Projects I just haven't named here yet. I also like to use Raspberry Pi as I work with Python. I have created various Pyton Scripts
+			as practice. Some examples are scripts to automated photos from a Raspi Camera, Simple Remote Control Car (using a car kit with 4 wheels/motors), and still various examples.
+                        <a href="https://retropie.org.uk/" Use target="_blank">Start
+                        here for more details.</a>
+                        </p>
+			</ul>
+	  	</div>
+	</div>
+	</div>
+	<div id="web_apps" class="tab_content">
+		<div class="card">
+	  		<div class="card-header">
+				<h3>Web Applications </h3>
+			</div>
+			<div class="card-body">
+				<ul style="list-style-type:none">
+				  <li><b>-></b> I have been working with Web Application Technologies for over 5 years. However my recent experience at Acquia has allowed me to have
+					a much deeper knowledge of how Web Applications work, how to set them up properly and correct develoment practices.
+				  </li>
+				  <li><b>-></b> I now understand how a Web Site propogates across the Internet, using DNS, CDN's and Load Balancers and Web Servers.</li>
+				  <li><b>-></b> As practice, I have setup many websites using CDN, SSL, redirects, mostly on Apache and Nginx.</li>
+				  <li><b>-></b> I have worked with many different Web Application Technologies and
+				  and languages. Most recentley I have been mostly using PHP, </>
+				</ul>
+		  	</div>
+		</div>
+	</div>
+
+
+	<div id="mobile_apps" class="tab_content">
+		<div class="card">
+	  		<div class="card-header">
+				<h3>Mobile Applications </h3>
+			</div>
+			<div class="card-body">
+				<ul style="list-style-type:none">
+				  <li><b>--></b>I have spent several years working on personal Mobile Application Projects, mostly using Android. This experience
+					  has helped me understand Mobile Application Life Cycles, proper practices laid out of the OS creators and more.
+				  </li>
+				  <li><b>--></b>I have created a fairly complex Android Application. I was able to incorporate Google Market Pay, and other
+					  libraries Android provides to tighting bind it with Android in the background.
+				  </li>
+				</ul>
+			</div>
+		</div>
+	</div>
 
 
 
 </div>
+
+
+
+<body>
+<script>
+	
+		function openEx(evt, cat) {
+		  var i, tabcontent, tablinks;
+		  tabcontent = document.getElementsByClassName("tab_content");
+		  for (i = 0; i < tabcontent.length; i++) {
+			tabcontent[i].style.display = "none";
+		  }
+		  tablinks = document.getElementsByClassName("tablinks");
+		  for (i = 0; i < tablinks.length; i++) {
+			tablinks[i].className = tablinks[i].className.replace(" active", "");
+		  }
+		  document.getElementById(cat).style.display = "block";
+		  evt.currentTarget.className += " active";
+		}
+		document.getElementById("test").click();
+</script>
+</html>
