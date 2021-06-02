e888f4ea1925d3338c9eeeb2bb8b63da969b1cd8
root <myhealthsheets>
2021-06-02 17:54:06 -0400

    adding first commit, working homepage

---
+++ docroot/home_tabs.php
@@ -0,0 +1,37 @@
+<div class="container-fluid" style="margin-top:15rem">
+	<div class="my_tabs">
+	  <button class="tablinks" onclick="openEx(event, 'Education')"id="test"><h4>General Experience</h4></button>
+	  <button class="tablinks" onclick="openEx(event, 'Work')"><h4>Raspberry Pi</h4></button>
+	  <button class="tablinks" onclick="openEx(event, 'Strengths')"><h4>Web Applications</h4></button>
+	  <button class="tablinks" onclick="openEx(event, 'Hobbies')"><h4>Mobile Applications</h4></button>
+	</div>
+
+
+	<div id="Education" class="tab_content">
+		<div class="card">
+			<h4 class="card-header">Software Programming Diploma</h4>
+			<div class="card-body">
+				 <p>I have always been interesting in Electronics and Computers. It probably started>
+		                      I often left them in pieces or put back together with one or more parts missing>
+		                  </p>
+		                  <p>
+		                      From there I worked in various industries utilizing these skills until realizin>                      wanted to do was work with Computers and Computer Software.
+		                  </p>
+		                  <p>
+		                    I received my Software Programming Diploma in 2012 and have been continuing to le>                    opportunity I get.
+		                  </p>
+		                <p>
+		                    The Electronics industry has changed consderably since I graduated in 2004, and t>                    and SoC circuits like Arduino and picoPY. I have created simple LED lights workin>                    projects with Bluetooth and Audio Amplifiers.
+		                </p>
+		                <p>
+		                    For a more fullstack programming experience I have created many projects with SBC>                    projects ranging from a Car Entertainment System, Open Source Home Automation Sof>                    with many others. The Raspberry Pi has also allowed me to work through the Comman>                    get forgetten.
+		                </p>	
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+</div>
