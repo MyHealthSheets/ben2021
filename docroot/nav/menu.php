fb3b18d3c6a81d6e4d1fe79402a53df1c52eda7d
root <root@Bedeadu-Razer.localdomain>
2021-06-04 22:35:46 -0400

    fies

---
+++ docroot/nav/menu.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-sm navbar-dark " style="background-color:#697184">
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+			    <span class="navbar-toggler-icon"></span>
+		    </button>
+
+		  <div class="collapse navbar-collapse " id="navbarsExample03">
+			<ul class="navbar-nav mr-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="https://www.myhealthsheets.com">MyHS (Web App)</a>
+			  </li>
+			</ul>
+		  </div>
+</nav>
