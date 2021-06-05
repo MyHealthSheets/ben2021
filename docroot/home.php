8059b14a0992f5dac17481b744010ec17546100c
root <myhealthsheets>
2021-06-04 23:06:19 -0400

    home.php now matches old index.php/html, no more issues

--- docroot/home.php
+++ docroot/home.php
@@ -6,6 +6,8 @@
     <?php include "nav/menu.php"; ?>
     <?php include "home/home_jumbo.php"; ?>
 <div class="main-body">
+ 	<?php include "home/home_main.php"; ?>
 </div>
 </body>
+	<?php include "nav/footer.php"; ?>
 </html>
--- docroot/home/home_main.php
+++ docroot/home/home_main.php
@@ -126,10 +126,3 @@
 			</ul>
 		</div>
 </div>
-
-<footer class="page-footer font-small blue" style="background:#697184;">
-  	<div class="footer-copyright text-center py-3">© 2021 Copyright:
-    		<a href="http://bencitren.com/">BenCitren.com</a>
-	</div>
-</footer>
-
