36c9e24c7c5596ac63245454ca83325893284865
root <root@Bedeadu-Razer.localdomain>
2021-06-04 22:49:53 -0400

    adding homejumbo

---
+++ docroot/home/home_jumbo.php
@@ -0,0 +1,31 @@
+<div class="jumbotron jumbrotron-fluid" style="border-radius:0px;border-top:2px solid #413F3D;padding-bottom:5rem;">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3">
+                <img src="images/me_ish.jpg" width="325px" height="375px";>
+            </div>	
+            <div class="col-lg-2">
+
+            </div>
+            <div class="col-lg-7 justify-content-center">
+                <h1><br><b>Benjamin Citren</b></h1><hr>
+                <pre>
+                <b>
+                _________________________________________
+                / Hello and welcome to my site! I created \
+                | this to document some of the current as |
+                | well as past technologies and projects  |
+                \ I have worked on.                       /
+                -----------------------------------------
+                    \
+                    \
+                        oO)-.                       .-(Oo
+                        /__  _\                     /_  __\
+                        \  \(  |     ()~()         |  )/  /
+                        \__|\ |    (-___-)        | /|__/
+                        '  '--'    ==`-'==        '--'  '
+                </b></pre>	
+            </div>
+        </div>
+    </div>
+</div>
