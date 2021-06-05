8f05fbdb4c1430dd12b8ec0a21c81e064d3aab0e
root <root@Bedeadu-Razer.localdomain>
2021-06-04 23:01:29 -0400

    more complete home.php

---
+++ docroot/nav/footer.php
@@ -0,0 +1,5 @@
+<footer class="page-footer font-small blue" style="background:#697184;">
+	<div class="footer-copyright text-center py-3">© 2021 Copyright:
+			<a href="http://bencitren.com/">BenCitren.com</a>
+	</div>
+</footer>
