df41adcb9ea8945ca6ea19e3501248e4058d5273
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2022-08-11 20:27:21 -0400

    updates accordion... agin

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -2,49 +2,136 @@
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-        Accordion Item #1
+General Backgroud
       </button>
     </h2>
     <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+	  <p class="card-text">I have always been interesting in Electronics and Computers. It probably started in my pre-teens, taking apart mine (and my family's) Electronic Devices to try and understand them better.
+		              I often left them in pieces or put back together with one or more parts missing. I think partly seeing these boxes of broken Electronics is what ultimately helped me decide to go to college and graduate with an Electronics Technologist Diploma. 
+		          </p>
+		          <p>
+		              From there I worked in various industries utilizing these skills until realizing what I really
+		              wanted to do was work with Computers and Computer Software.
+		          </p>
+		          <p>
+		            I received my Software Programming Diploma in 2012 and have been continuing to learn new technologies every
+		            opportunity I get.
+		          </p>
+		        <p>	
+		            The Electronics industry has changed consderably since I graduated in 2004, and this was the dawn of IoT technologies
+		            and SoC circuits like Arduino and picoPY. I have created simple LED lights working with sensors and timers, and more complex
+		            projects with Bluetooth and Audio Amplifiers. 
+		        </p>
+		        <p>
+		            For a more fullstack programming experience I have created many projects with SBC's, like the Raspberry Pi. I have 
+		            projects ranging from a Car Entertainment System, Open Source Home Automation Software, Drupal and PHP Web Applications, along
+		            with many others. The Raspberry Pi has also allowed me to work through the Command Line and making sure my skills don't 
+		            get forgetten.
+		        </p>      </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-        Accordion Item #2
-      </button>
+Recent Raspberry Pi Projects      </button>
     </h2>
     <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
       <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+	  <ul style="list-style-type:none">
+	            	<li> <h5>OpenHAB (Smart Home Server)</h5>
+	        	        <p> Open Source PHP Web Application is a tool to setup and control Smart Home
+	                	devices from a Web Browser. <br><a href="https://www.openhab.org/" Use target="_blank"> Click this link to learn more about OpenHAB</a>
+	                	</p><hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">
+	                </li>    
+		        <li>
+		                <h5>Personal PHP Web site projects </h5>
+		                <p>I have been developing a couple PHP based Web sites/applications for practice.
+		                This site is a simple PHP application, however I also have a demo Web Application for myhealthsheets.com. I do most of my dev work on my Raspberry Pi, then push this to a Web Server when ready for production.
+		                </p> 
+		                <hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">
+	            	</li>
+	            	<li>
+		                <h5>Personal Ebook Server using Calibre</h5>
+		                <p>Browse, Download, Upload my book library and read direct from Web Browser
+		                </p> 
+	            	</li><hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">  
+	            	<li>
+				    <h5>Owncloud Server</h5>
+					<p> Personal File Server similar to Dropbox and other Applications. This allows me to share files with family and friends without any free tier storage size limitations.
+	            	</li><hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">
+
+		    	<li>
+                		<h5>Druapal Web Application</h5>
+                            	<p>Created some basic Drupal Applications for practice. 
+	                    		<a href="https://www.drupal.org/" Use target="_blank"> Visit Drupal.org for
+	                    		more details.</a>
+                            	</p>
+			</li>
+                    		<hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">
+                    	<li>
+	                	<h5>MiniDLNA, Plex and Kodi Media Servers</h5>
+	                        <p>I worked various technologies like this in order to share media content across my home network. These applications allow any TV or monitor attached to the network to 
+	                        browse and watch any type of Media Content. <a href="https://pimylifeup.com/raspberrypi-minidlna/" Use target="_blank">This link will take you to a great doc on how to setup miniDLNA
+	                        more details.</a>
+	                        </p>
+	            	</li><hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">
+	            	<li>
+		                <h5>RetroPIE - EmulationStation for Raspberry Pi</h5>
+		                <p>This turns any Raspi into a retro-gaming machine. Allows you to play just about
+		                all retro gaming consoles through your raspberry Pi.This is a fun project that you can
+		                also setup with NES or other type cases and utilize BT joysticks that look like
+		                retro controllers. 
+	                            <a href="https://retropie.org.uk/" Use target="_blank">Start
+		                here for more details.</a>
+	                            </p>
+	            	</li><hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">
+    		    	<li>
+	                	<h5>Python Scripts and other General Command Line Tools</h5>
+	                            <p>I have worked and am working on many other Projects I just haven't named here yet. I also like to use Raspberry Pi as I work with Python. I have created various Pyton Scripts
+	                	as practice. Some examples are scripts to automated photos from a Raspi Camera, Simple Remote Control Car (using a car kit with 4 wheels/motors), and still various examples.
+	                            <a href="https://retropie.org.uk/" Use target="_blank">Start
+	                            here for more details.</a>
+	                            </p>
+    		    	</li><hr style="background-color:#FF8D3F;margin: 1rem 5rem 1rem 5rem;">
+        	    	</ul>      </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-	        Accordion Item #3
+	        Web Applications
       </button>
     </h2>
     <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+	  <ul>
+	            <li><b></b> I have been working with Web Application Technologies for over 5 years. However my recent experience at Acquia has allowed me to have
+	            a much deeper knowledge of how Web Applications work, how to set them up properly and correct develoment practices.
+	            </li>
+	            <li><b></b> I now understand how a Web Site propogates across the Internet, using DNS, CDN's and Load Balancers and Web Servers.</li>
+	            <li><b></b> As practice, I have setup many websites using CDN, SSL, redirects, mostly on Apache and Nginx.</li>
+	            <li><b></b> I have worked with many different Web Application Technologies and
+	            and languages. Most recentley I have been mostly using PHP, </>
+	        </ul>      </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="panelsStayOpen-headingFour">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
-	        Accordion Item #4
+	        Mobile Applications
       </button>
     </h2>
     <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingFour">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
+	  <ul style=>
+				  <li><b></b>I have spent several years working on personal Mobile Application Projects, mostly using Android. This experience
+					  has helped me understand Mobile Application Life Cycles, proper practices laid out of the OS creators and more.
+				  </li>
+				  <li><b></b>I have created a fairly complex Android Application. I was able to incorporate Google Market Pay, and other
+					  libraries Android provides to tighting bind it with Android in the background.
+				  </li>
+				</ul>      </div>
     </div>
   </div>
 </div>
