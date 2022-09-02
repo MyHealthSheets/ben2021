4cfa59212ce9d48bada12da38bec474ee00a1b6e
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2022-09-01 21:07:42 -0400

    test322

--- docroot/home/home_jumbo.php
+++ docroot/home/home_jumbo.php
@@ -1,4 +1,4 @@
- <div class="p-5 text-center" style="background-color:#756867">
+ <div class="p-4 text-center">
 <!--   <div class="jumbotron jumbrotron-fluid" style="border-radius:0px;border-top:3px solid #D5D6D2;padding-bottom:5rem;">
  -->    <div class="container-fluid">
         <div class="row">
@@ -10,7 +10,7 @@
             </div>
             <div class="col-lg-7 justify-content-center">
                 <h1><br><b>Benjamin Citren</b></h1><hr/>
-                <pre style="color:#D5D6D2;border-top:0.25px solid #FF8D3F;">
+                <pre style="color:#D5D6D2;border-top:0.25px solid #B19F9E;">
 
      /\--------------------/\
      |			    |
