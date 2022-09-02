113f5ca63e082c0fe4ce5790730f4c17332af51f
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2022-09-01 23:28:20 -0400

    adding back original Jumbo Brown1

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -100,35 +100,40 @@
 			</div>
 
 			<div class="card">
-				<div class="card-header">
-					<h2>Web Apps </h2>
-				</div>
-				<div class="card-body" style="text-align:left;">
-					<ul>
-						<li><b></b> I have been working with Web Application Technologies for over 5 years. However my recent experience at Acquia has allowed me to have
-						a much deeper knowledge of how Web Applications work, how to set them up properly and correct develoment practices.
-						</li>
-						<li><b></b> I now understand how a Web Site propogates across the Internet, using DNS, CDN's and Load Balancers and Web Servers.</li>
-						<li><b></b> As practice, I have setup many websites using CDN, SSL, redirects, mostly on Apache and Nginx.</li>
-						<li><b></b> I have worked with many different Web Application Technologies and
-						and languages. Most recentley I have been mostly using PHP, </>
-					</ul>
+				<a class="card-header" data-bs-toggle="collapse" href="#WebCollapse" role="button" aria-expanded="false" aria-controls="WebCollapse">
+						<h2>Web Apps</h2>
+				</a>
+				<div class="collapse multi-collapse" id="WebCollapse">	
+					<div class="card card-body" style="text-align:left;">
+						<ul>
+							<li><b></b> I have been working with Web Application Technologies for over 5 years. However my recent experience at Acquia has allowed me to have
+							a much deeper knowledge of how Web Applications work, how to set them up properly and correct develoment practices.
+							</li>
+							<li><b></b> I now understand how a Web Site propogates across the Internet, using DNS, CDN's and Load Balancers and Web Servers.</li>
+							<li><b></b> As practice, I have setup many websites using CDN, SSL, redirects, mostly on Apache and Nginx.</li>
+							<li><b></b> I have worked with many different Web Application Technologies and
+							and languages. Most recentley I have been mostly using PHP, </>
+						</ul>
+					</div>
 				</div>
 			</div>
 
 			<div class="card">
-					<div class="card-header">
-						<h2>Mobile Apps </h2>
+				<div class="card-header">
+					<a class="card-header" data-bs-toggle="collapse" href="#MobileCollapse" role="button" aria-expanded="false" aria-controls="MobileCollapse">
+						<h2>Mobile Apps</h2>
+					</a>
+					<div class="collapse multi-collapse" id="MobileCollapse">
+						<div class="card-body" style="text-align:left;">
+							<ul>
+								<li><b></b>I have spent several years working on personal Mobile Application Projects, mostly using Android. This experience
+									has helped me understand Mobile Application Life Cycles, proper practices laid out of the OS creators and more.
+								</li>
+								<li><b></b>I have created a fairly complex Android Application. I was able to incorporate Google Market Pay, and other
+									libraries Android provides to tighting bind it with Android in the background.
+								</li>
+							</ul>
+						</div>
 					</div>
-					<div class="card-body" style="text-align:left;">
-						<ul>
-						<li><b></b>I have spent several years working on personal Mobile Application Projects, mostly using Android. This experience
-							has helped me understand Mobile Application Life Cycles, proper practices laid out of the OS creators and more.
-						</li>
-						<li><b></b>I have created a fairly complex Android Application. I was able to incorporate Google Market Pay, and other
-							libraries Android provides to tighting bind it with Android in the background.
-						</li>
-						</ul>
-					</div>
+				</div>
 			</div>
-	</div>
