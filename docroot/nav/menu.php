057d941e124bf4755a31b258391c1b4cb79921a1
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2022-09-02 21:02:27 -0400

    chgs to home ui, first chgs on branch home_ui_changes

--- docroot/nav/menu.php
+++ docroot/nav/menu.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-sm navbar-dark">
-		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+		    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#benNav" aria-controls="benNav" aria-expanded="false" aria-label="Toggle navigation">
 			    <span class="navbar-toggler-icon"></span>
 		    </button>
 
-		  <div class="collapse navbar-collapse " id="navbarsExample03">
+		  <div class="collapse navbar-collapse " id="benNav">
 			<ul class="navbar-nav mr-auto">
 			  <li class="nav-item active">
 				<a class="nav-link" href="#">Home <span class="sr-only"></span></a>
@@ -14,6 +14,6 @@
 			  	<li class="nav-item">
 				<a class="nav-link" href="#">Other</a>
 			  </li>
-			</ul>
+			</ul>	
 		  </div>
 </nav>
