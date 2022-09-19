bef9e215e8df682b90d94dc55dbdc4d575447ada
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2022-09-19 18:56:57 -0400

    I think main/home was updated

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -6,17 +6,19 @@
 				<div class="collapse multi-collapse" id="GenCollapse">
 					<div class="card card-body">
 						<p class="card-text">
-							I have backgrounds in both Electronics and Computer Programming. 
-
+							I have backgrounds in both Electronics and Computer Programming.
+						</p>
+						<p>
 							I do my best to keep up with and learn new technologies, and am always working on 
 							projects in my spare time to achieve this. I spent several years developing an Android Application,
 							"MyHealthSheets". In order to get more familiar with other programming languages and frameworks, I have redeveloped 
 							this Android App and used it as a skeleton for other projects.
-
+						</p>
+						<p>
 							I continue working on a variety Electronics projects, and with the advent of IoT technologies have been
 							able to combine my two passions of Electronics and Development. I have an arsenal of Arduino, ESP32, SMC's, and Electronics 
 							Components to work with.
-
+						</p>
 							My other projects revolve around Web Servers, both on the cloud and utilizing hardware like the Raspberry Pi.
 						</p>
 					</div>
