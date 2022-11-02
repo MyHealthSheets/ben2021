56d9f456221a6da3013a2ffef70c198dd04cb7ed
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2022-11-02 19:15:32 -0400

    testing new navbar

--- docroot/nav/menu.php
+++ docroot/nav/menu.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#BenMenu" aria-controls="BenMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <div class="collapse navbar-collapse" id="BenMenu">
 	<img src="../images/raps/rap_claw.PNG" alt="" width="30" height="24" class="d-inline-block align-text-top">
 	<a class="navbar-brand" href="#">Ben</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -11,16 +11,9 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="https://www.myhealthsheets.com">MyHS (Web App)</a>
         </li>
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
