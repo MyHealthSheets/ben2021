d9905a84ae6fe56a47a0e595f9dad44dddbeafb1
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2023-01-04 17:50:55 -0500

    udpate main page to use tabs

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -1,4 +1,20 @@
 <div class="container">
+	<ul class="nav nav-tabs">
+	<li class="nav-item">
+		<a class="nav-link active" aria-current="page" href="#">General Background</a>
+		<?php include "content/gen.php"; ?>	
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="#">Link</a>
+		<?php include "content/gen.php"; ?>		
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="#">Link</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link disabled">Disabled</a>
+	</li>
+	</ul>
 	<?php include "content/gen.php"; ?>			
 	<?php include "content/raspi.php"; ?>
 	<?php include "content/webApps.php"; ?>
