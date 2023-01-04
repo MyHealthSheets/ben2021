d4a846b2ec2bd9a798e4fec191486b9afd233285
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2023-01-04 17:53:55 -0500

    udpate main page to use tabs-reverted

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -1,20 +1,4 @@
 <div class="container">
-	<ul class="nav nav-tabs">
-	<li class="nav-item">
-		<a class="nav-link active" aria-current="page" href="#">General Background</a>
-		<?php include "content/gen.php"; ?>	
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="#">Link</a>
-		<?php include "content/gen.php"; ?>		
-	</li>
-	<li class="nav-item">
-		<a class="nav-link" href="#">Link</a>
-	</li>
-	<li class="nav-item">
-		<a class="nav-link disabled">Disabled</a>
-	</li>
-	</ul>
 	<?php include "content/gen.php"; ?>			
 	<?php include "content/raspi.php"; ?>
 	<?php include "content/webApps.php"; ?>
