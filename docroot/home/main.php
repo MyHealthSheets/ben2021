2520308927aa4be80065c3a139f881c6818a66d5
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2023-01-05 20:26:56 -0500

    test card div splits

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -1,8 +1,12 @@
 <div class="container">
-	<?php include "content/gen.php"; ?>			
+	<?php include "content/gen.php"; ?>		
+	<div class="col-md-6">	
 	<?php include "content/raspi.php"; ?>
+</div>
 	<?php include "content/webApps.php"; ?>
+	<div class="col-md-6">	
 	<?php include "content/mobileApps.php"; ?>
+	</div>
 </div>
 
 
