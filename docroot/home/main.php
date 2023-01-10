2b08d696de7b373e6cddd236cbfe5d8f34b671f2
bedeadu <myhealthsheets@gmail.com>
2023-01-10 18:56:33 -0500

    add modded mobile apps content

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -2,12 +2,20 @@
         <?php include "content/gen.php"; ?>
         <div class="row justify-content-center">
         <div class="col-sm-6">
+<<<<<<< HEAD
                         <?php include "content/mobileApps.php"; ?>
+=======
+			<?php include "content/mobileApps.php"; ?>
+>>>>>>> b51d0cb (update with columns on main.php)
         </div>
         <div class="col-sm-6">
                 <?php include "content/webApps.php"; ?>
         </div>
         </div>
+<<<<<<< HEAD
                                 <?php include "content/raspi.php"; ?>
+=======
+				<?php include "content/raspi.php"; ?>
+>>>>>>> b51d0cb (update with columns on main.php)
 
 </div>
