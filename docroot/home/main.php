5460ba3712cfc3abaa4770271b791f48bf7ec006
myhealthsheets <myhealthsheets.com>
2023-01-10 23:10:20 +0000

    update

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -1,13 +1,13 @@
 <div class="container">
-	<?php include "content/gen.php"; ?>		
-	<div class="col-md-6">	
-	<?php include "content/raspi.php"; ?>
-</div>
-	<?php include "content/webApps.php"; ?>
-	<div class="col-md-6">	
-	<?php include "content/mobileApps.php"; ?>
-	</div>
-</div>
+        <?php include "content/gen.php"; ?>
+        <div class="row justify-content-center">
+        <div class="col-sm-6">
+                        <?php include "content/mobileApps.php"; ?>
+        </div>
+        <div class="col-sm-6">
+                <?php include "content/webApps.php"; ?>
+        </div>
+        </div>
+                                <?php include "content/raspi.php"; ?>
 
-
-			
+</div>
