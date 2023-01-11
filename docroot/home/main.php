b26a2856159dc09aa42213e58c0ea995f0ebf7dd
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2023-01-11 18:55:15 -0500

    updates gen.php

--- docroot/home/main.php
+++ docroot/home/main.php
@@ -1,10 +1,10 @@
 <div class="container">
         <?php include "content/gen.php"; ?>
         <div class="row justify-content-center">
-        <div class="col-sm-6">
+        <div class="col-lg-6">
                         <?php include "content/mobileApps.php"; ?>
         </div>
-        <div class="col-sm-6">
+        <div class="col-lg-6">
                 <?php include "content/webApps.php"; ?>
         </div>
                                 <?php include "content/raspi.php"; ?>
