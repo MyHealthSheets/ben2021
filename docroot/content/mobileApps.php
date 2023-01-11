5be5d4a3a1e7f97536acb3c53095b3fc9c4d9b5f
Ben Citren <bencitren@Bens-MacBook-Pro.local>
2023-01-11 18:46:52 -0500

    updates gen.php

--- docroot/content/mobileApps.php
+++ docroot/content/mobileApps.php
@@ -3,15 +3,15 @@
             <h2>Mobile Apps</h2>
         </a>
         <div class="collapse multi-collapse" id="MobileCollapse">
-            <div class="card-body" style="text-align:left;">
+            <div class="card-body">
                 <ul>
-                    <li><b></b>I have built a fairly extensive Android Application from scratch. 
+                    <li>I have built a fairly extensive Android Application from scratch. 
                     </li>
-                    <li><b></b>I mostly used Google's Android Documentation to learn how to program an Android Application using best practices.
+                    <li>I mostly used Google's Android Documentation to learn how to program an Android Application using best practices.
                     </li>
-                    <li><b></b>This was before Gogole switched to Kotlin, so it was built with Java, using a SQLite Database</li>
+                    <li>This was before Gogole switched to Kotlin, so it was built with Java, using a SQLite Database</li>
                 
-                    <li><b></b>Although I used the Android Developer Portal for Alpha and Beta testing, I never actually released it publicly. My mistake in early development was deciding to use a local database. In retrospect, I realized in order to sell the app with a paid monthly subscription I needed to use a cloud database implementation, but by this time I started working on web applications and never went back to update the app.
+                    <li>Although I used the Android Developer Portal for Alpha and Beta testing, I never actually released it publicly. My mistake in early development was deciding to use a local database. In retrospect, I realized in order to sell the app with a paid monthly subscription I needed to use a cloud database implementation, but by this time I started working on web applications and never went back to update the app.
                 </ul>
             </div>
         </div>
