50079c75c955554fd545bc24c9fbf9ee568f7c89
root <root@pi-42>
2024-01-10 12:19:35 -0500

    update footer

--- docroot/nav/footer.php
+++ docroot/nav/footer.php
@@ -1,5 +1,5 @@
 <footer class="page-footer font-small blue"">
-	<div class="footer-copyright text-center py-3">© 2023 Copyright:
-			<a href="https://bencitren.com/">BenCitren.com</a>
+	<div class="footer-copyright text-center py-3">© 2024 Copyright:
+			<a href="https://bencitren.com/">BenCitren.com BEN IS AWESOME - JAN 2024</a>
 	</div>
 </footer>
